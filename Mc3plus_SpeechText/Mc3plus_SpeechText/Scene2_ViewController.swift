ccb3f9ec30499479ab33e610a80b5fcb8235f7a1
kwekkwek <41714182+kwekkwek@users.noreply.github.com>
2018-11-21 10:32:58 +0700

    Add files via upload
    Initial Commit

---
+++ Mc3plus_SpeechText/Mc3plus_SpeechText/Scene2_ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Scene2_ViewController.swift
+//  Mc3plus_SpeechText
+//
+//  Created by Benny Kurniawan on 21/11/18.
+//  Copyright © 2018 Benny Kurniawan. All rights reserved.
+//
+
+import UIKit
+
+class Scene2_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Mc3plus_SpeechText/Mc3plus_SpeechText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mc3plus_SpeechText
+//
+//  Created by Benny Kurniawan on 21/11/18.
+//  Copyright © 2018 Benny Kurniawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
