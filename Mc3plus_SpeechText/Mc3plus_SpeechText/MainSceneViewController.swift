f96b4b27e7da01d689302d48ea9b9242a33e25fb
Yuvens Putra Barata <yuvens.barata@gmail.com>
2018-11-21 15:22:05 +0700

    Adding sound and speech for main scene

--- Mc3plus_SpeechText/Mc3plus_SpeechText/MainSceneViewController.swift
+++ Mc3plus_SpeechText/Mc3plus_SpeechText/MainSceneViewController.swift
@@ -7,16 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
-class MainSceneViewController: UIViewController {
-    var swipeDown:UISwipeGestureRecognizer?
+class MainSceneViewController: UIViewController, AVSpeechSynthesizerDelegate {
     
+    //MARK: Variables
+    var swipeDown:UISwipeGestureRecognizer?
+    var sound: AVAudioPlayer?
+    let textNarration = "Welcome to Black Out, where your auditory is challenged. Please listen carefully and follow all the given instructions to finish the task. Just remember, seeing is not everything."
+    
+    let textInstruction = "This is your first task, find a flashlight for your brother. It is located at the study room. Now, swipe down to open your door."
+    
+    //MARK: LifeCycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+
+        soundsInit()
+        speech(x: textNarration + textInstruction)
         swipethisDown()
     }
+    
+//MARK: Function
+    
+    //MARK: Gesture Function
     func swipethisDown() {
         swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(Handler))
         swipeDown?.direction = .down
@@ -25,16 +39,34 @@ class MainSceneViewController: UIViewController {
     }
     @objc func Handler()
     {
-        print("swipe")
+        sound?.play()
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    //MARK: Sounds Function
+    func soundsInit() {
+        let soundURL = URL.init(fileURLWithPath: Bundle.main.path(forResource: "My Movie", ofType: "wav")!)
+        
+        do {
+            try sound = AVAudioPlayer(contentsOf: soundURL)
+            sound?.prepareToPlay()
+        }
+        catch {
+            print("error: \(error.localizedDescription)")
+        }
     }
-    */
+    
+    //MARK: Speech Function
+    func speech(x: String) {
+        let speechText = AVSpeechUtterance(string: x)
+        
+        speechText.voice = AVSpeechSynthesisVoice (language: "ind-INA")
+        speechText.rate = 0.5
+        speechText.pitchMultiplier = 1
+        speechText.preUtteranceDelay = 0
+        
+        let speaker = AVSpeechSynthesizer()
+        speaker.speak(speechText)
+        speechText.postUtteranceDelay = 0.003
+    }
 
 }
